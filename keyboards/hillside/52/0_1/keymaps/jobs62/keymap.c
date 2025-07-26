c737937c50e1d2c6e623fc07d5b2b14b5322e231
Ch.-David Blot <droper@analphabet.me>
2025-07-26 15:13:02 +0200

    numpad v1

--- keyboards/hillside/52/0_1/keymaps/jobs62/keymap.c
+++ keyboards/hillside/52/0_1/keymaps/jobs62/keymap.c
@@ -6,12 +6,14 @@
 enum layers {
     _AZERTY = 0,
     _NUM,
+    _NUMPAD,
 };
 
 #define xxxxxxx KC_NO
 
 #define LY_NAV OSL(_NUM)
 #define RY_NAV MO(_NUM)
+#define RY_NUM MO(_NUMPAD)
 #define ALT_GR OSM(MOD_RALT)
 #define OSM_SFT OSM(MOD_LSFT)
 
@@ -32,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,  KC_A,    KC_Z,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
      KC_TAB,  MKC_Q,   MKC_S,   MKC_D,   MKC_F,   KC_G,                                           KC_H,    MKC_J,   MKC_K,   MKC_L,   MKC_M,   KC_QUOT,
      _______, KC_W,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LBRC,                       KC_RBRC, KC_N,    KC_SCLN, KC_COMM, KC_DOT,  KC_UP,   KC_EQL,
-     _______, KC_BSLS, ALT_GR,          _______, LY_NAV,  OSM_SFT, KC_ENT,        KC_SPC, RY_NAV, KC_MINS, KC_SLSH,          KC_LEFT, KC_DOWN, KC_RGHT
+     _______, KC_BSLS, ALT_GR,           RY_NUM, LY_NAV,  OSM_SFT, KC_ENT,        KC_SPC, RY_NAV, KC_MINS, KC_SLSH,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_NUM] = LAYOUT(
      _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                         KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_PGUP,
@@ -40,4 +42,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_LPRN,                       KC_RPRN, KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_TRNS,
      KC_F11 , KC_F12 , KC_LCTL,          _______, _______, _______, _______,     _______, _______, KC_GRV, _______,          KC_RCTL, KC_HOME, KC_END
     ),
+    [_NUMPAD] = LAYOUT(
+     _______, _______, _______, _______, _______, _______,                                        _______, KC_7   , KC_8   , KC_9   , KC_KP_PLUS, KC_PGUP,
+     _______, _______, _______, _______, _______, _______,                                        _______, KC_4   , KC_5   , KC_6   , KC_KP_MINUS, KC_PGDN,
+     _______, _______, _______, _______, _______, _______, _______,                      _______, _______, KC_1   , KC_2   , KC_3   , KC_KP_ENT, _______,
+     _______, _______, _______,          _______, _______, _______, _______,    _______, _______, _______, _______,          _______, _______, _______
+    ),
 };
+
+//  Template:
+//    [_INDEX] = LAYOUT(
+//     _______, _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______, _______,
+//     _______, _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______, _______,
+//     _______, _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______, _______,
+//     _______, _______, _______,          _______, _______, _______, _______,    _______, _______, _______, _______,          _______, _______, _______
+//    ),
